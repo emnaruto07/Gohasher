1eaf1d9e55cbff17604d4ad1611ac995a7417cb4
emnaruto07 <shazebvps@gmail.com>
2021-04-17 03:24:37 +0530

    Started

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	ParseOptions()
+	fmt.Println(banner)
+}
+
+const Version = `v1.0`
+
+const banner = `_________     ______               ______              
+__  ____/________  /_______ __________  /______________
+_  / __ _  __ \_  __ \  __  /_  ___/_  __ \  _ \_  ___/
+/ /_/ / / /_/ /  / / / /_/ /_(__  )_  / / /  __/  /    
+\____/  \____//_/ /_/\__,_/ /____/ /_/ /_/\___//_/v1.0 `
+
+type Options struct {
+	Hash        string
+	List        string
+	Concurrency int
+	Version     bool
+}
+
+func ParseOptions() *Options {
+	options := &Options{}
+
+	flag.StringVar(&options.Hash, "hash", "", "Provide the hash string with this flag.")
+	flag.StringVar(&options.List, "l", "", "Provide the file, Which contain the hashes.")
+	flag.IntVar(&options.Concurrency, "c", 10, "Number of concurrent goroutines for resolving")
+	flag.BoolVar(&options.Version, "version", false, "Show the version of GoHasher.")
+	flag.Parse()
+
+	if options.Version {
+		fmt.Println("[+]Current Version:", Version)
+		os.Exit(0)
+	}
+
+	return options
+}
+
+func alpha(hashvalue string, hashtype string) {
+	return false
+}
+func beta(hashvalue string, hashtype string) {
+	resp, err := http.Get("https://hashtoolkit.com/reverse-hash/?hash=" + hashvalue)
+}
